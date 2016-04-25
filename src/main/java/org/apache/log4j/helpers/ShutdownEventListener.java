6fa73dc745cdc79ed8a89d04c5e27523f7455f6b
Victor Zeng <Victor.Zxy@outlook.com>
2016-04-25 16:43:27 +0800

    fix the issus #4913 and #41214

---
+++ src/main/java/org/apache/log4j/helpers/ShutdownEventListener.java
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.log4j.helpers;
+
+/**
+ * Interface to receive a notification when the LoggerRepository is being shut
+ * down. Used to terminate the {@link FileWatchdog} thread.
+ *
+ * @since 1.2.17-patched
+ * 
+ * @author Victor Zeng
+ */
+
+public interface ShutdownEventListener {
+
+	/**
+	 * Called when the LoggerRepository is being shut down.
+	 */
+	public void shutdown();
+}
